e030fb652568880e08a9894f60c8ba6e8b33f36a
Liel Almog <70017134+liel-almog@users.noreply.github.com>
2022-01-30 22:16:37 +0200

    Create a first view of leaflet map

--- src/components/leaflet/leaflet.tsx
+++ src/components/leaflet/leaflet.tsx
@@ -1,11 +1,24 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { LatLngExpression } from "leaflet";
+import { useMemo } from "react";
+import { MapContainer, Polygon, TileLayer } from "react-leaflet";
+import json from "../../../neighbourhoods.json";
 import classes from "./leaflet.module.scss";
 
 const Leaflet = () => {
+  const polygonOptions = { color: "white", fillColor: "yellow" };
+
+  const multiPolygon = useMemo(() => {
+    return json.features.map((feature) => {
+      return feature.geometry.coordinates[0].map((coordinate) => {
+        return [coordinate[1], coordinate[0]];
+      });
+    }) as LatLngExpression[][];
+  }, []);
+
   return (
     <MapContainer
-      center={[32.02, 34.778]}
-      zoom={13}
+      center={[32.077210, 34.799939]}
+      zoom={13.2}
       scrollWheelZoom={true}
       className={classes.mapContainer}
     >
@@ -13,11 +26,7 @@ const Leaflet = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[32.02, 34.778]}>
-        <Popup>
-          A Popup <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <Polygon pathOptions={polygonOptions} positions={multiPolygon} />
     </MapContainer>
   );
 };

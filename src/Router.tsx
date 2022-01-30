ada8d0df187a5bae9541c6cb19e996910f774887
Liel Almog <70017134+liel-almog@users.noreply.github.com>
2022-01-30 22:16:16 +0200

    Create Router file

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,10 @@
 import { Navigate, Route, Routes } from "react-router";
-import "./App.css";
-import HomePage from "./pages/HomePage/HomePage";
+import App from "./pages/App";
 
 export default function Router() {
   return (
     <Routes>
-      <Route path="/home" element={<HomePage />} />
+      <Route path="/home" element={<App />} />
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>
   );
